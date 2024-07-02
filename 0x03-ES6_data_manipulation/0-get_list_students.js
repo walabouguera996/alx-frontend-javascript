460929389e7e8425446f8709c6ca03100c10d620
walabouguera996 <walabouguera996@gmail.com>
2024-07-02 20:33:26 +0000

    last commit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,14 @@
-export default function getListStudents() {
-  return [
+// 0-get_list_students.js: Basic list of objects
+
+function getListStudents() {
+  // create an array of student objects
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return students;
 }
+
+export default getListStudents;
